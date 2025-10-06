989687a6c6a803a7190f717e1dcaa943c8f6d337
dhick4-creator <andika@hatiibs.sch.id>
2025-10-06 09:12:21 +0000

    Fix title in dashboard.php to reflect correct application name

--- dashboard.php
+++ dashboard.php
@@ -11,11 +11,7 @@ if (!isset($_SESSION['username']) || ($_SESSION['role'] !== 'admin' && $_SESSION
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-  <title>Dashboard Admin Absensi Sholat Dhuhur QR</title>
-=======
   <title>Dashboard Admin Absensi Jamaah QR</title>
->>>>>>> 221b6cccffe028aa08e087dedef1b34cc07599ab
   <!-- Font Awesome Free CDN -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
